d356090fab1645124d6c8a7f873c9f206d6ee1de
sinilya <ilyasinizin@forquest.ru>
2018-04-30 19:18:01 +0300

    update #1

--- init.js
+++ init.js
@@ -2,4 +2,8 @@ var express = require('express');
 
 var app = express();
 
+app.get("/", function(request, response){
+    response.send("<h2>NodeJS Express server by sinilya</h2>");
+});
+
 app.listen(3000);
