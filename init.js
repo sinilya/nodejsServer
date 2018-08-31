7381f3a1f0ee8820deb3a3a67837bc1ca0c3da90
sinilya <ilyasinizin@forquest.ru>
2018-08-31 19:17:56 +0300

    update

--- init.js
+++ init.js
@@ -1,11 +1,4 @@
 var express = require('express');
-var couchbase = require('couchbase');
-
-var cluster = new couchbase.Cluster('couchbase://127.0.0.1/'); 
-cluster.authenticate('Administrator', 'testpassword');
-var bucket = cluster.openBucket('testBucket');
-var N1qlQuery = couchbase.N1qlQuery;
-
 var app = express();
 
 app.get("/", function(req, res){
@@ -13,26 +6,8 @@ app.get("/", function(req, res){
 });
 
 app.get("/db", function(req, res){
-	bucket.upsert('testdoc', {testField: "db is working!"}, function(err, result) {
-		if(err){
-			console.log(err);
-		}
 
-		bucket.get('testdoc', function(err, result) {
-			if (err) {
-				console.log(err);
-			}
-
-			console.log(result.value);
-		});
-	});
-});
-
-app.get('/close', function(req, res){
-	console.log("Closing server...");
-	server.close();
-	process.exit();
-})
+}
 
 var server = app.listen(3000, function(){
 	console.log("Server started on port " + server.address().port);
